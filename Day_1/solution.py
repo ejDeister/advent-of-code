8e88dcd7bf0fcaee8d64105cfc19b4e976638ce1
ejDeister <ethandeister@gmail.com>
2024-12-03 01:52:11 -0800

    Solved Day 1 Part 2

--- Day_1/solution.py
+++ Day_1/solution.py
@@ -1,4 +1,5 @@
 with open("advent-of-code\\Day_1\\input.txt", encoding="utf-8") as input_file:
+    #Part 1
     TOTAL = 0
     nums1, nums2 = [], []
     for line in input_file:
@@ -9,5 +10,15 @@ with open("advent-of-code\\Day_1\\input.txt", encoding="utf-8") as input_file:
     nums2.sort()
     for num1, num2 in zip(nums1, nums2):
         TOTAL += abs(int(num1)-int(num2))
-    print(TOTAL)
+    print("Total:",TOTAL)
 
+    #Part 2
+    unique_nums1 = set()
+    frequencies = {}
+    for num in nums1:
+        unique_nums1.add(num)
+    for num in nums2:
+        if num in unique_nums1:
+            frequencies[num] = frequencies.get(num, 0) + int(num)
+    sim_score = sum(freqs for freqs in frequencies.values())
+    print("Similarity Score:", sim_score)
